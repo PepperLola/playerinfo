c3f8e42762aeee0a002053fa8a446d15591e4cea
PepperLola <julian.orion.wright@gmail.com>
2020-05-30 16:38:44 -0700

    fixed note block helper so it wouldn't show high F# three times and not show low F# at all

--- src/main/java/com/palight/playerinfo/modules/NoteBlockUtil.java
+++ src/main/java/com/palight/playerinfo/modules/NoteBlockUtil.java
@@ -4,10 +4,11 @@ import net.minecraftforge.event.world.NoteBlockEvent;
 
 public class NoteBlockUtil {
     public static int noteId = -1;
+    public static int maxLength = (NoteBlockEvent.Note.values().length * 2) + 1;
 
     public static void setNoteId(int noteId) {
         if (noteId != NoteBlockUtil.noteId) {
-            NoteBlockUtil.noteId = noteId % (NoteBlockEvent.Note.values().length * 2);
+            NoteBlockUtil.noteId = noteId % maxLength;
         }
     }
 
@@ -17,7 +18,7 @@ public class NoteBlockUtil {
 
     public static NoteBlockEvent.Note getNote(int id) {
         NoteBlockEvent.Note[] notes = NoteBlockEvent.Note.values();
-        return notes[id % (notes.length * 2)];
+        return notes[id % maxLength];
     }
 
     public static String getNoteString(NoteBlockEvent.Note note) {
