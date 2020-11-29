d8854a2f47ed6c89f6cffce397a688fd3c7a06d8
PepperLola <julian.orion.wright@gmail.com>
2020-11-28 21:06:54 -0800

    started working on module filtering, although it really lags the game out

--- src/main/java/com/palight/playerinfo/gui/screens/impl/MainGui.java
+++ src/main/java/com/palight/playerinfo/gui/screens/impl/MainGui.java
@@ -13,6 +13,8 @@ import com.palight.playerinfo.gui.widgets.impl.GuiTexturedButton;
 import com.palight.playerinfo.modules.Module;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.BlockPos;
@@ -41,7 +43,9 @@ public class MainGui extends CustomGuiScreenScrollable {
     private GuiTexturedButton settingsGuiButton;
     private GuiButton editModsButton;
 
-    List<GuiModuleEntry> modules = new ArrayList<GuiModuleEntry>();
+    private GuiTextField filterTextInput;
+
+    List<GuiModuleEntry> modules = new ArrayList<>();
 
     public MainGui() {
         super(I18n.format("screen.main"));
@@ -57,8 +61,46 @@ public class MainGui extends CustomGuiScreenScrollable {
 
         columnWidth = ((xSize - (3 * leftOffset)) - (2 * hPadding)) / columns;
 
-        for (int i = 0; i < PlayerInfo.getModules().values().size(); i++) {
-            Module module = new ArrayList<Module>(PlayerInfo.getModules().values()).get(i);
+        ScaledResolution res = new ScaledResolution(Minecraft.getMinecraft());
+
+        this.filterTextInput = new GuiTextField(0, Minecraft.getMinecraft().fontRendererObj, (this.width + xSize) / 2 - 128, (this.height - ySize) / 2 - 18, 128, 18);
+
+        renderModules();
+
+        this.menuBar = new GuiMenuBar(1, (width - xSize) / 2 + 67, (height - ySize) / 2 + 5, 180, 16, new CustomGuiScreen[]{new InfoGui(), new ServerSelector(), new LifxGui(), new CalculatorGui()});
+
+        this.settingsGuiButton = new GuiTexturedButton(2, (width - xSize) / 2 + 226 - 24, (this.height + ySize) / 2 - 24, 20, 20, 0, 0);
+        this.editModsButton = new GuiButton(3, guiX + leftOffset + 2, (height + ySize) / 2 - 24, 48, 20, "Edit GUI");
+
+        this.buttonList.addAll(Arrays.asList(
+                this.settingsGuiButton,
+                this.editModsButton
+        ));
+    }
+
+    private void renderModules() {
+        this.drawDefaultBackground();
+        this.mc.getTextureManager().bindTexture(gui);
+        drawTexturedModalRect(guiX, guiY, 0, 0, xSize, ySize);
+
+        modules.clear();
+
+        List<Module> filteredModules = new ArrayList<>();
+
+        System.out.println("FILTER TEXT: " + filterTextInput.getText());
+        if (filterTextInput.getText().equals("")) {
+            filteredModules.addAll(PlayerInfo.getModules().values());
+        } else {
+            for (Module module : PlayerInfo.getModules().values()) {
+                String filterText = filterTextInput.getText().toLowerCase();
+                if (module.getName().toLowerCase().contains(filterText) || module.getDescription().toLowerCase().contains(filterText)) {
+                    filteredModules.add(module);
+                }
+            }
+        }
+
+        for (int i = 0; i < filteredModules.size(); i++) {
+            Module module = filteredModules.get(i);
             int xPosition = guiX + leftOffset + hPadding + (columnWidth * (i % columns));
             int yPosition = guiY + headerHeight + (i - (i % columns)) * rowHeight + 4;
             GuiModuleEntry entry = new GuiModuleEntry(this, i, module, xPosition, yPosition, columnWidth, rowHeight);
@@ -66,22 +108,21 @@ public class MainGui extends CustomGuiScreenScrollable {
             modules.add(entry);
         }
 
-
-        this.menuBar = new GuiMenuBar(0, (width - xSize) / 2 + 67, (height - ySize) / 2 + 5, 180, 16, new CustomGuiScreen[]{new InfoGui(), new ServerSelector(), new LifxGui(), new CalculatorGui()});
-
-        this.settingsGuiButton = new GuiTexturedButton(3, (width - xSize) / 2 + 226 - 24, (this.height + ySize) / 2 - 24, 20, 20, 0, 0);
-        this.editModsButton = new GuiButton(4, guiX + leftOffset + 2, (height + ySize) / 2 - 24, 48, 20, "Edit GUI");
-
-        this.buttonList.addAll(Arrays.asList(
-                this.settingsGuiButton,
-                this.editModsButton
-        ));
-
+        this.guiElements.clear();
         this.guiElements.addAll(modules);
     }
 
     @Override
-    protected void actionPerformed(GuiButton b) throws IOException {
+    protected void keyTyped(char typed, int modifier) throws IOException {
+        super.keyTyped(typed, modifier);
+        if (filterTextInput.isFocused()) {
+            filterTextInput.textboxKeyTyped(typed, modifier);
+            renderModules();
+        }
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton b) {
         System.out.println(b.id);
 
         EntityPlayer player = Minecraft.getMinecraft().thePlayer;
@@ -105,16 +146,30 @@ public class MainGui extends CustomGuiScreenScrollable {
         this.drawDefaultBackground();
         super.drawScreen(mouseX, mouseY, partialTicks);
         menuBar.drawWidget(mc, mouseX, mouseY);
+        this.filterTextInput.drawTextBox();
     }
 
     @Override
     protected void mouseClicked(int x, int y, int btn) throws IOException {
         menuBar.mouseClicked(x, y);
+        filterTextInput.mouseClicked(x, y, btn);
         super.mouseClicked(x, y, btn);
     }
 
     @Override
     public boolean doesGuiPauseGame() {
         return false;
+    }
+
+    public static void openGui() {
+        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        World playerWorld = player.getEntityWorld();
+        BlockPos playerLocation = player.getPosition();
+        player.openGui(PlayerInfo.instance, GuiHandler.MAIN_GUI_ID, playerWorld, playerLocation.getX(), playerLocation.getY(), playerLocation.getZ());
+    }
+
+    public static void closeGui() {
+        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        player.closeScreen();
     }
 }
--- src/main/java/com/palight/playerinfo/listeners/KeyListener.java
+++ src/main/java/com/palight/playerinfo/listeners/KeyListener.java
@@ -1,12 +1,8 @@
 package com.palight.playerinfo.listeners;
 
-import com.palight.playerinfo.PlayerInfo;
-import com.palight.playerinfo.gui.GuiHandler;
+import com.palight.playerinfo.gui.screens.impl.MainGui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
@@ -25,10 +21,7 @@ public class KeyListener {
             String key_id = keybind.getKeyDescription();
             System.out.println(key_id);
             if (key_id.equals("key.main")) {
-                EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-                World playerWorld = player.getEntityWorld();
-                BlockPos playerLocation = player.getPosition();
-                player.openGui(PlayerInfo.instance, GuiHandler.MAIN_GUI_ID, playerWorld, playerLocation.getX(), playerLocation.getY(), playerLocation.getZ());
+                MainGui.openGui();
             }
         }
     }
