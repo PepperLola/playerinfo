52eecc9801a91bde0d91b7c3549663d34a3452a4
PepperLola <julian.orion.wright@gmail.com>
2021-03-11 22:50:53 -0800

    added clock module

--- src/main/java/com/palight/playerinfo/PlayerInfo.java
+++ src/main/java/com/palight/playerinfo/PlayerInfo.java
@@ -29,7 +29,7 @@ public class PlayerInfo
     //TODO update version here and in build.gradle
     public static final String NAME = "playerinfo";
     public static final String MODID = "playerinfo";
-    public static final String VERSION = "1.16.31";
+    public static final String VERSION = "1.16.32";
     public static String commitHash;
     public static String defaultBranchName = "master";
     public static String githubAPIURL = "https://api.github.com/repos/PepperLola/playerinfo/git/refs/heads/" + defaultBranchName;
@@ -81,6 +81,7 @@ public class PlayerInfo
         modules.put("keystrokes", new KeystrokesMod());
         modules.put("textReplacement", new TextReplacementMod());
         modules.put("fullBright", new FullBrightMod());
+        modules.put("clock", new ClockMod());
     }
 
     @EventHandler
