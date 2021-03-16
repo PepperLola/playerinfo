8a0edad1b60a4f0ee65bf4d5cb1519515bb2d65b
PepperLola <julian.orion.wright@gmail.com>
2021-03-16 12:43:44 -0700

    updated README and enhanced time changer mod by letting you lock it to day time, night time, bounce between day/night start and end (sin wave) and set it to real time, which uses your computer's time

--- src/main/java/com/palight/playerinfo/gui/screens/impl/options/modules/misc/TimeChangerGui.java
+++ src/main/java/com/palight/playerinfo/gui/screens/impl/options/modules/misc/TimeChangerGui.java
@@ -4,6 +4,7 @@ import com.palight.playerinfo.PlayerInfo;
 import com.palight.playerinfo.gui.screens.CustomGuiScreenScrollable;
 import com.palight.playerinfo.gui.widgets.GuiCustomWidget;
 import com.palight.playerinfo.gui.widgets.impl.GuiButton;
+import com.palight.playerinfo.gui.widgets.impl.GuiCheckBox;
 import com.palight.playerinfo.gui.widgets.impl.GuiDropdown;
 import com.palight.playerinfo.modules.impl.misc.TimeChangerMod;
 import com.palight.playerinfo.options.ModConfiguration;
@@ -20,8 +21,13 @@ public class TimeChangerGui extends CustomGuiScreenScrollable {
 
     private GuiDropdown timePicker;
     private GuiButton setTimeButton;
+    private GuiCheckBox lockDay;
+    private GuiCheckBox lockNight;
+    private GuiCheckBox bounce;
 
     private GuiSlider fastTimeMultiplierSlider;
+
+    private TimeChangerMod module;
 
     public TimeChangerGui() {
         super("screen.timeChanger");
@@ -30,6 +36,10 @@ public class TimeChangerGui extends CustomGuiScreenScrollable {
     @Override
     public void initGui() {
         super.initGui();
+
+        if (module == null) {
+            module = (TimeChangerMod) PlayerInfo.getModules().get("timeChanger");
+        }
 
         buttonX = guiX + 32;
         buttonY = guiY + 32;
@@ -44,11 +54,18 @@ public class TimeChangerGui extends CustomGuiScreenScrollable {
         timePicker = new GuiDropdown(0, buttonX, buttonY, times);
         setTimeButton = new GuiButton(1, buttonX + 64, buttonY, 64, 20, "Set Time");
 
-        fastTimeMultiplierSlider = new GuiSlider(2, buttonX, buttonY + 32, 128, 20, "Time Multiplier: ", "", 0D, 4D, ((TimeChangerMod) PlayerInfo.getModules().get("timeChanger")).fastTimeMultiplier, true, true);
+        lockDay = new GuiCheckBox(2, buttonX, buttonY + 64, "Lock Day", module.lockDay);
+        lockNight = new GuiCheckBox(3, buttonX, buttonY + 96, "Lock Night", module.lockNight);
+        bounce = new GuiCheckBox(4, buttonX, buttonY + 128, "Bounce", module.bounce);
+
+        fastTimeMultiplierSlider = new GuiSlider(2, buttonX, buttonY + 32, 128, 20, "Time Multiplier: ", "", 0D, 4D, module.fastTimeMultiplier, true, true);
 
         this.guiElements.addAll(Arrays.asList(
                 this.timePicker,
-                this.setTimeButton
+                this.setTimeButton,
+                this.lockDay,
+                this.lockNight,
+                this.bounce
         ));
     }
 
@@ -62,9 +79,15 @@ public class TimeChangerGui extends CustomGuiScreenScrollable {
     protected void widgetClicked(GuiCustomWidget widget) {
         super.widgetClicked(widget);
         if (widget.id == setTimeButton.id) {
-            ((TimeChangerMod) PlayerInfo.getModules().get("timeChanger")).selectedTime = timePicker.getSelectedItem();
-            ModConfiguration.syncFromGUI();
+            module.selectedTime = timePicker.getSelectedItem();
+        } else if (widget.id == lockDay.id) {
+            module.lockDay = lockDay.checked;
+        } else if (widget.id == lockNight.id) {
+            module.lockNight = lockNight.checked;
+        } else if (widget.id == bounce.id) {
+            module.bounce = bounce.checked;
         }
+        ModConfiguration.syncFromGUI();
     }
 
     @Override
@@ -78,7 +101,7 @@ public class TimeChangerGui extends CustomGuiScreenScrollable {
     protected void mouseReleased(int p_mouseReleased_1_, int p_mouseReleased_2_, int p_mouseReleased_3_) {
         super.mouseReleased(p_mouseReleased_1_, p_mouseReleased_2_, p_mouseReleased_3_);
         fastTimeMultiplierSlider.mouseReleased(p_mouseReleased_1_, p_mouseReleased_2_);
-        ((TimeChangerMod) PlayerInfo.getModules().get("timeChanger")).fastTimeMultiplier = fastTimeMultiplierSlider.sliderValue;
+        module.fastTimeMultiplier = fastTimeMultiplierSlider.sliderValue;
         ModConfiguration.syncFromGUI();
     }
 }
