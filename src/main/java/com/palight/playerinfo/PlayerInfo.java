760c81b732c6a000add788538d23ce3d6f7c7703
PepperLola <julian.orion.wright@gmail.com>
2021-06-14 19:12:48 -0700

    update version

--- src/main/java/com/palight/playerinfo/PlayerInfo.java
+++ src/main/java/com/palight/playerinfo/PlayerInfo.java
@@ -34,7 +34,7 @@ public class PlayerInfo
     //TODO update version here and in build.gradle
     public static final String NAME = "playerinfo";
     public static final String MODID = "playerinfo";
-    public static final String VERSION = "1.17.6";
+    public static final String VERSION = "1.17.8";
     public static String commitHash;
     public static String defaultBranchName = "master";
     public static String githubAPIURL = "https://api.github.com/repos/PepperLola/playerinfo/git/refs/heads/" + defaultBranchName;
@@ -92,6 +92,7 @@ public class PlayerInfo
         modules.put("memory", new MemoryMod());
         modules.put("potions", new PotionsMod());
         modules.put("screenshotHelper", new ScreenshotHelperMod());
+        modules.put("playerHider", new PlayerHiderMod());
     }
 
     @EventHandler
