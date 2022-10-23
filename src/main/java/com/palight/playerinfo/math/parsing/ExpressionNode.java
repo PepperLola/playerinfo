59967ceb2a9daeb2cf492ca15a0da50b4ce8fe68
PepperLola <julian.orion.wright@gmail.com>
2022-10-23 14:03:36 -0700

    fixed calculator parsing

--- src/main/java/com/palight/playerinfo/math/parsing/ExpressionNode.java
+++ src/main/java/com/palight/playerinfo/math/parsing/ExpressionNode.java
@@ -1,99 +1,128 @@
 package com.palight.playerinfo.math.parsing;
 
 import com.palight.playerinfo.math.MathOperator;
-import org.apache.commons.lang3.ArrayUtils;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Queue;
 
 public abstract class ExpressionNode {
 
-    public static String operators = "(?<=[0-9.])[^0-9.\\n\\r]";
-    public static String numbers = "((?<![0-9.])[-][0-9.]+)|[0-9.]+";
-    public static String functions = "((log|sin|cos|tan|cot|sec|csc|asin|acos|atan|sqrt)\\([0-9.,]+\\))";
+    public static String operators = "(?<=[0-9.])[+-/\\^*]";
+//    public static String numbers = "((?<![0-9.])[-][0-9.]+)|[0-9.]+";
+//    public static String functions = "((log|sin|cos|tan|cot|sec|csc|asin|acos|atan|sqrt)\\(.+\\))";
 
     public static double evaluateExpression(String expression) throws InvalidExpressionException, ImaginaryNumberException {
         return parseExpression(expression).evaluate();
     }
 
-    private static ExpressionNode parseExpression(String exp) throws InvalidExpressionException, ImaginaryNumberException {
-        exp = exp.replace(" ", "");
-
-        exp = parseFunctions(exp);
-
-        String[] numberArray = ArrayUtils.removeElement(exp.split(operators), "");
-        String[] operatorArray = ArrayUtils.removeElement(exp.replaceAll(numbers, "").split(""), "");
-
-        Queue<String> nums = new LinkedList<String>(Arrays.asList(numberArray));
-        Queue<String> ops = new LinkedList<String>(Arrays.asList(operatorArray));
-
-        ExpressionNode node = createExpressionTree(null, nums, ops);
-
-        System.out.println(node.toString());
-        return node;
+    private static boolean isValue(String exp) {
+        try {
+            Double.parseDouble(exp);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
-    private static String parseFunctions(String exp) throws ImaginaryNumberException {
-        List<String> matches = new ArrayList<>();
-        Matcher m = Pattern.compile(functions).matcher(exp);
-        while (m.find()) {
-            matches.add(m.group());
+    public static int findNextOperator(String s, int startIndex) {
+        for (int i = startIndex; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                    return i;
+            }
         }
-        for (String match : matches) {
-            String function = match.replaceAll("[^a-zA-Z]", "");
-            String argString = match.replaceAll("[^0-9,.\\-]+", "");
-            String[] args = argString.split(",");
-            String result = String.valueOf(parseFunction(function, args));
-            exp = exp.replace(match, result);
-        }
-        return exp;
+
+        return -1;
     }
 
-    private static double parseFunction(String function, String[] args) throws ImaginaryNumberException {
-        double value = Double.parseDouble(args[0]);
-        double result = 0;
-        switch (function) {
-            case "log":
-                double base = Double.parseDouble(args[1]);
-                result =  Math.log(value) / Math.log(base);
-                break;
-            case "sin":
-                result =  Math.sin(value);
-                break;
-            case "cos":
-                result =  Math.cos(value);
-                break;
-            case "tan":
-                result =  Math.tan(value);
-                break;
-            case "cot":
-                result =  1 / Math.tan(value);
-                break;
-            case "atan":
-                result =  Math.atan(value);
-                break;
-            case "sec":
-                result =  1 / Math.cos(value);
-                break;
-            case "acos":
-                result =  Math.acos(value);
-                break;
-            case "csc":
-                result =  1 / Math.sin(value);
-                break;
-            case "asin":
-                result =  Math.asin(value);
-                break;
-            case "sqrt":
-                result =  Math.sqrt(value);
-                break;
+    private static int findClosingParenthesis(String exp, int startIndex) {
+        int counter = 0;
+        for (int i = startIndex; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (c == ')' && counter == 0) return i;
+            else if (c == '(') counter ++;
+            else if (c == ')') counter --;
         }
-        if (Double.isNaN(result)) {
-            throw new ImaginaryNumberException();
-        }
-        return result;
+
+        return -1;
     }
+
+//    private static ExpressionNode parseEquation(String exp) throws InvalidExpressionException, ImaginaryNumberException {
+//        System.out.println("PARSING EXPRESSION " + exp);
+//        exp = exp.replace(" ", "");
+//
+//        exp = parseFunctions(exp);
+//
+//        String[] numberArray = ArrayUtils.removeElement(exp.split(operators), "");
+//        String[] operatorArray = ArrayUtils.removeElement(exp.replaceAll(numbers, "").split(""), "");
+//
+//        Queue<String> nums = new LinkedList<>(Arrays.asList(numberArray));
+//        Queue<String> ops = new LinkedList<>(Arrays.asList(operatorArray));
+//
+//        ExpressionNode node = createExpressionTree(null, nums, ops);
+//
+//        System.out.println(node);
+//        return node;
+//    }
+
+    private static ExpressionNode parseExpression(String exp) {
+        if (isValue(exp)) return new ValueNode(exp);
+
+        char first = exp.charAt(0);
+        if (first >= '0' && first <= '9') {
+            int operatorIdx = findNextOperator(exp, 0);
+            ValueNode value = new ValueNode(exp.substring(0, operatorIdx));
+            MathOperator operator = MathOperator.fromString("" + exp.charAt(operatorIdx));
+            ExpressionNode right = ExpressionNode.parseExpression(exp.substring(operatorIdx + 1));
+
+            return new OperatorNode(value, right, operator);
+        }
+        if (first == '(') {
+            int nextCloseIdx = findClosingParenthesis(exp, 1);
+            ExpressionNode parenExpression = ExpressionNode.parseExpression(exp.substring(1, nextCloseIdx));
+            if (exp.length() <= nextCloseIdx + 1) return parenExpression;
+
+            int nextOperatorIdx = findNextOperator(exp, nextCloseIdx);
+            MathOperator operator = MathOperator.fromString("" + exp.charAt(nextOperatorIdx));
+            ExpressionNode right = ExpressionNode.parseExpression(exp.substring(nextOperatorIdx + 1));
+
+            return new OperatorNode(parenExpression, right, operator);
+        }
+        // otherwise is function
+        int firstParenIdx = exp.indexOf('(');
+        String functionName = exp.substring(0, firstParenIdx);
+        int closingParenIdx = findClosingParenthesis(exp, firstParenIdx + 1);
+        ExpressionNode functionContents = ExpressionNode.parseExpression(exp.substring(firstParenIdx + 1, closingParenIdx));
+        FunctionNode functionNode = new FunctionNode(functionName, functionContents);
+
+        if (exp.length() <= closingParenIdx + 1) return functionNode;
+
+        int nextOperatorIdx = findNextOperator(exp, closingParenIdx);
+        MathOperator operator = MathOperator.fromString("" + exp.charAt(nextOperatorIdx));
+        ExpressionNode right = ExpressionNode.parseExpression(exp.substring(nextOperatorIdx + 1));
+
+        return new OperatorNode(functionNode, right, operator);
+    }
+
+//    private static String parseFunctions(String exp) throws ImaginaryNumberException, InvalidExpressionException {
+//        List<String> matches = new ArrayList<>();
+//        Matcher m = Pattern.compile(functions).matcher(exp);
+//        while (m.find()) {
+//            matches.add(m.group());
+//        }
+//        for (String match : matches) {
+//            String function = match.replaceAll("[^a-zA-Z]", "");
+//            String argString = match.replaceAll("[^0-9,.\\-]+", "");
+//            String[] args = argString.split(",");
+//            String result = String.valueOf(parseFunction(function, args));
+//            exp = exp.replace(match, result);
+//        }
+//        return exp;
+//    }
 
     private static ExpressionNode createExpressionTree(ExpressionNode leftNode, Queue<String> nums, Queue<String> ops) throws InvalidExpressionException {
         if (ops.size() == 0 && nums.size() == 1) {
@@ -127,6 +156,8 @@ public abstract class ExpressionNode {
 
         MathOperator nextOperator = MathOperator.fromString(ops.peek());
 
+        System.out.println("OPERATOR: " + operator + " | NEXT OPERATOR: " + nextOperator);
+
         if (operator.getPrecedence() > nextOperator.getPrecedence()) {
             if (nums == null || nums.size() <= 0) {
                 throw new InvalidExpressionException();
@@ -143,9 +174,9 @@ public abstract class ExpressionNode {
         return operatorNode;
     }
 
-    protected abstract double evaluate();
+    protected abstract double evaluate() throws ImaginaryNumberException;
 
     public static void main(String[] args) throws InvalidExpressionException, ImaginaryNumberException {
-        System.out.println(evaluateExpression("36^(1/2)"));
+        System.out.println(evaluateExpression("sqrt(2+2)"));
     }
 }
